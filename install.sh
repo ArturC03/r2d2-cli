d355b2a7b8afb143a31ff5cec6d87f8e905c50a8
ArturC03 <arturvicentecruz@proton.me>
2025-07-06 18:52:22 +0100

    feat/chore: changing the install script

--- install.sh
+++ install.sh
@@ -2,31 +2,30 @@
 
 set -e
 
-# Criar diretório temporário
-TMP_DIR=$(mktemp -d)
-echo "Cloning R2D2 CLI to $TMP_DIR..."
-git clone https://github.com/ArturC03/r2d2-cli.git "$TMP_DIR"
-
-# Ir para o diretório
-cd "$TMP_DIR"
-
-# Construir o instalador
-echo "Building installer..."
-go build -o installer/build/r2d2-installer ./installer
-
-# Caminho para o binário
-INSTALLER_PATH="$TMP_DIR/installer/build/r2d2-installer"
-
-# Verifica se é root
+# Detecta se está a correr como root
 if [ "$(id -u)" -ne 0 ]; then
-  echo "Este script precisa de sudo. A reiniciar com sudo..."
-  exec sudo "$0" "$@"
+  echo "Cloning R2D2 CLI..."
+
+  TMP_DIR=$(mktemp -d)
+  git clone https://github.com/ArturC03/r2d2-cli.git "$TMP_DIR"
+  cd "$TMP_DIR"
+
+  echo "Building installer..."
+  go build -o installer/build/r2d2-installer ./installer
+
+  INSTALLER_PATH="$TMP_DIR/installer/build/r2d2-installer"
+
+  echo "Re-running as sudo..."
+  exec sudo bash -c "INSTALLER_PATH='$INSTALLER_PATH' bash -s" < "$0"
 fi
 
-# Executar o instalador
+# Parte que corre como root
+echo "Running installer as root..."
 chmod +x "$INSTALLER_PATH"
 "$INSTALLER_PATH"
 
-# Limpar
-echo "Cleaning up..."
-rm -rf "$TMP_DIR"
+# Limpeza só se o tmp ainda existir
+if [ -d "$(dirname "$INSTALLER_PATH")/../.." ]; then
+  echo "Cleaning up..."
+  rm -rf "$(dirname "$INSTALLER_PATH")/../.."
+fi
